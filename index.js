7fe0ed3bfc2dbf0fd7e8d2dddecc46cb106c8daa
Michelle Di Guglielmo <mfdigug@gmail.com>
2025-10-02 14:47:33 +1000

    added pokemon name to main display using fetch for renderPokemon function

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const pokemonCard = document.querySelector(".pokemon-card")
-
+const pokemonList = document.querySelector("#pokemon-list")
+const pokemonName = document.querySelector("#pokemon-name")
 
 
 function renderPokemon(pokemon) {
-   document.querySelector("#pokemon-name").innerText = pokemon[0].name
-   document.querySelector("p").innerText = pokemon[0].type
+   pokemonName.innerText = pokemon[0].name;
+   
 }
 
+function listPokemon(pokemon) {
+   for (let i = 0; i < pokemon.length; i++) {
+   const li = document.createElement("li");
+   li.innerText = pokemon[i].name;
+   pokemonList.appendChild(li);
+   }  
+}
+
+fetch("http://localhost:3000/pokemon")
+.then(res => res.json())
+.then(pokemon => listPokemon(pokemon))
 
 fetch("http://localhost:3000/pokemon")
 .then(res => res.json())
