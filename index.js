80e0bdfa3366ff75b463e0f5a8124f8022bb7264
Michelle Di Guglielmo <mfdigug@gmail.com>
2025-10-04 11:45:29 +1000

    added dataset id to each li for listPokemon function in index.js

--- index.js
+++ index.js
@@ -3,23 +3,31 @@ const pokemonList = document.querySelector("#pokemon-list")
 const pokemonName = document.querySelector("#pokemon-name")
 
 
-function renderPokemon(pokemon) {
-   pokemonName.innerText = pokemon[0].name;
-   
-}
+
 
 function listPokemon(pokemon) {
    for (let i = 0; i < pokemon.length; i++) {
    const li = document.createElement("li");
    li.innerText = pokemon[i].name;
+   li.dataset.id = pokemon[i].id
    pokemonList.appendChild(li);
-   }  
+   
+
+   li.addEventListener('click', (e) => console.log(e))
+   }   
+   //function fetchPokemonforDisplay(pokemonForDisplay) {
+   //   fetch(`http://localhost:3000/pokemon/${pokemonForDisplay}`)
+   //   .then(res => res.json())
+    //  .then(displayPokemon => console.log(displayPokemon))
+   
+   //}
+
+   //function renderPokemon(pokemonForDisplay) {
+   //   pokemonName.innerText = pokemonForDisplay;
+   //}
+
 }
 
 fetch("http://localhost:3000/pokemon")
 .then(res => res.json())
 .then(pokemon => listPokemon(pokemon))
-
-fetch("http://localhost:3000/pokemon")
-.then(res => res.json())
-.then(pokemon => renderPokemon(pokemon))
